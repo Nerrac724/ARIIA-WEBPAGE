5995f0f72dc3f71106b45b284f82048aef058454
Nerrac724 <78541742+Nerrac724@users.noreply.github.com>
2025-07-04 17:51:50 -0400

    Fix ARIIAPortal import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
-import { ARIIAPortal } from './components/ARIIAPortal';
+import { ARIIAPortal } from './components/ARIIAPortal.tsx';
 
 function App() {
   return (
